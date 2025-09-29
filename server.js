6be1f33d7d44869bfa8e732d7cf5dc0928df8b4c
godwinkalu <kalugodwin158@gmail.com>
2025-09-29 16:47:30 +0100

    allowed cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 require("./config/database")
+const cors = require('cors')
 const userRouter  = require('./routes/userRouter')
 const port = process.env.port || 8999
 
 const app = express()
 app.use(express.json())
+app.use(cors('*'))
 app.use(userRouter)
 
 app.listen(port, ()=>{
